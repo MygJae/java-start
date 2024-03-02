3dd1cc6dfbf4b3bb9c574c0f7f8c8563f653dcdd
Mygjae <50861514+MygJae@users.noreply.github.com>
2024-03-02 11:17:27 +0900

    Hello World

---
+++ src/CommentJava.java
@@ -0,0 +1,14 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("CommentJava1"); //CommentJava 출력
+//        System.out.println("CommentJava2");
+        /*
+        System.out.println("CommentJava3");
+        System.out.println("CommentJav4");
+        */
+    }
+
+
+}
+
